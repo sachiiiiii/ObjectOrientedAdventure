6d7a2cc24bf0647235e7f2cb59ae892e91d3ba9c
Osi <osachi752@gmail.com>
2025-03-23 22:10:17 -0400

    completed part 1

--- script.js
+++ script.js
@@ -9,9 +9,28 @@ const adventurer = {
     inventory: ["sword", "potion", "artifact"],
     companion: {
         name: "Leo",
-        type: "Cat"
-    }
+        type: "Cat",
+        companion: {
+            name: "Frank",
+            type: "Flea",
+            belongings: ["small hat", "sunglasses"]
+        }
+    },
+    roll (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
+        }
 }
 adventurer.inventory.forEach(item => {
     console.log(item);
 });
+
+adventurer.roll(1);
+adventurer.roll();
+adventurer.roll();
+
+
+/**
+ * Part 2: Class Fantasy
+ */
+console.log("\nPart 2: Class Fantasy");
