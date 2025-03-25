bffca4f9573505f8f694c6b87e6170505b89bcd5
Osi <osachi752@gmail.com>
2025-03-24 22:08:04 -0400

    completed part 2

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ const adventurer = {
             belongings: ["small hat", "sunglasses"]
         }
     },
-    roll (mod = 0) {
+    roll(mod = 0) {
         const result = Math.floor(Math.random() * 20) + 1 + mod;
         console.log(`${this.name} rolled a ${result}.`)
-        }
+    }
 }
 adventurer.inventory.forEach(item => {
     console.log(item);
@@ -34,3 +34,47 @@ adventurer.roll();
  * Part 2: Class Fantasy
  */
 console.log("\nPart 2: Class Fantasy");
+
+class Character {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+    }
+    roll(mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
+    }
+}
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+robin.roll();
+robin.companion.roll();
+robin.companion.companion.roll();
+
+// class Adventurer extends Character {
+//     constructor(name, role, hasCompanions) {
+//         super(name);
+//         // Adventurers have specialized roles and companions.
+//         this.role = role;
+//         this.hasCompanion = hasCompanions.
+//         // Every adventurer starts with a bed and 50 gold coins.
+//         this.inventory.push("bedroll", "50 gold coins");
+//     }
+//     // Adventurers have the ability to scout ahead of them.
+//     scout() {
+//         console.log(`${this.name} is scouting ahead...`);
+//         super.roll();
+//     }
+// }
+
+// class Companion extends Adventurer {
+//     //constructor()
+// }
